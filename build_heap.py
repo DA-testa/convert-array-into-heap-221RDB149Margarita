1008b1ffde171192cb630653dc5b5f6068f372bf
Margarita <124924807+221RDB149Margarita@users.noreply.github.com>
2023-03-18 23:55:51 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,51 +1,35 @@
-# python3
+def parent(i):
+    return (i-1)//2
 
-def heapify(arr, n, i, swaps):
-    """
-    Heapify subtree rooted at index i.
-    """
-    
-    l = 2 * i + 1
-    r = 2 * i + 2 
+def left_child(i):
+    return 2*i + 1
 
-    if l < n and arr[l] > arr[largest]:
-        largest = l
+def right_child(i):
+    return 2*i + 2
 
-    if r < n and arr[r] > arr[largest]:
-        largest = r
-
-    if largest != i:
-        swaps.append((i, largest))
-        arr[i], arr[largest] = arr[largest], arr[i] 
-
-        heapify(arr, n, largest, swaps)
+def sift_down(i, arr, swaps):
+    min_idx = i
+    l = left_child(i)
+    if l < len(arr) and arr[l] < arr[min_idx]:
+        min_idx = l
+    r = right_child(i)
+    if r < len(arr) and arr[r] < arr[min_idx]:
+        min_idx = r
+    if i != min_idx:
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        swaps.append((i, min_idx))
+        sift_down(min_idx, arr, swaps)
 
 def build_heap(arr):
     swaps = []
-    n = len(arr)
-
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i, swaps)
-
+    for i in range(len(arr)//2, -1, -1):
+        sift_down(i, arr, swaps)
     return swaps
 
-def main():
-    source = input()
-    if "F" in source:
-        filename = input()
-        with open("tests/" + filename, 'r', encoding = "utf-8") as f:
-            n = int(f.readline())
-            arr = list(map(int, f.readline().split()))
-    elif "I" in source:
-        n = int(input())
-        arr = list(map(int, input().split()))
-    else:
-        exit()
-    assert len(arr) == n
-    swaps = build_heap(arr)
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+n = int(input())
+arr = list(map(int, input().split()))
+swaps = build_heap(arr)
 
-if __name__ == "__main__":
-    main()
+print(len(swaps))
+for i, j in swaps:
+    print(i, j)
