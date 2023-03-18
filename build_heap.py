1ba794995382ac54facd972e2744ba068c5d73ee
Margarita <124924807+221RDB149Margarita@users.noreply.github.com>
2023-03-19 00:02:51 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,34 +1,27 @@
-def parent(i):
-    return (i-1)//2
+def sift_down(n, arr, swaps):
+    i = 0
+    while 2*i+1 < n:
+        j = i
+        if arr[2*i+1] < arr[j]:
+            j = 2*i+1
+        if 2*i+2 < n and arr[2*i+2] < arr[j]:
+            j = 2*i+2
+        if i != j:
+            swaps.append((i, j))
+            arr[i], arr[j] = arr[j], arr[i]
+            i = j
+        else:
+            break
 
-def left_child(i):
-    return 2*i + 1
-
-def right_child(i):
-    return 2*i + 2
-
-def sift_down(i, arr, swaps):
-    min_idx = i
-    l = left_child(i)
-    if l < len(arr) and arr[l] < arr[min_idx]:
-        min_idx = l
-    r = right_child(i)
-    if r < len(arr) and arr[r] < arr[min_idx]:
-        min_idx = r
-    if i != min_idx:
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        swaps.append((i, min_idx))
-        sift_down(min_idx, arr, swaps)
-
-def build_heap(arr):
+def build_heap(n, arr):
     swaps = []
-    for i in range(len(arr)//2, -1, -1):
-        sift_down(i, arr, swaps)
+    for i in range(n//2, -1, -1):
+        sift_down(n, arr, swaps, i)
     return swaps
 
 n = int(input())
 arr = list(map(int, input().split()))
-swaps = build_heap(arr)
+swaps = build_heap(n, arr)
 
 print(len(swaps))
 for i, j in swaps:
