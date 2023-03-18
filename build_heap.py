013a55bcacacaa65a0b5d9a3fa155586770f2ea3
Margarita <124924807+221RDB149Margarita@users.noreply.github.com>
2023-03-19 00:10:06 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,28 +1,32 @@
-def sift_down(n, arr, swaps):
-    i = 0
-    while 2*i+1 < n:
-        j = i
-        if arr[2*i+1] < arr[j]:
-            j = 2*i+1
-        if 2*i+2 < n and arr[2*i+2] < arr[j]:
-            j = 2*i+2
-        if i != j:
-            swaps.append((i, j))
-            arr[i], arr[j] = arr[j], arr[i]
-            i = j
-        else:
-            break
+def sift_down(data, i, swaps):
+    """Sift down the element at index i in the data list to maintain heap property."""
+    n = len(data)
+    min_index = i
+    l = 2 * i + 1
+    if l < n and data[l] < data[min_index]:
+        min_index = l
+    r = 2 * i + 2
+    if r < n and data[r] < data[min_index]:
+        min_index = r
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        sift_down(data, min_index, swaps)
 
-def build_heap(n, arr):
+
+def build_heap(data):
+    """Build a min-heap from the given data list using O(n) swaps."""
     swaps = []
-    for i in range(n//2, -1, -1):
-        sift_down(n, arr, swaps, i)
+    n = len(data)
+    for i in range(n // 2 - 1, -1, -1):
+        sift_down(data, i, swaps)
     return swaps
 
-n = int(input())
-arr = list(map(int, input().split()))
-swaps = build_heap(n, arr)
 
-print(len(swaps))
-for i, j in swaps:
-    print(i, j)
+if __name__ == '__main__':
+    n = int(input())
+    data = list(map(int, input().split()))
+    swaps = build_heap(data)
+    print(len(swaps))
+    for swap in swaps:
+        print(swap[0], swap[1])
