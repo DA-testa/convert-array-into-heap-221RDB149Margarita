f9b30ac9de4e2c2762a64b66f9610a71c58a2ad2
Margarita <124924807+221RDB149Margarita@users.noreply.github.com>
2023-03-20 01:15:23 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,45 +1,54 @@
-def heapify(arr, n, i, swaps):
-    largest = i 
-    l = 2 * i + 1 
-    r = 2 * i + 2  
-    if l < n and arr[l] > arr[largest]:
-        largest = l
-    if r < n and arr[r] > arr[largest]:
-        largest = r
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]  
-        swaps.append((i, largest))
-        heapify(arr, n, largest, swaps)
- 
-def heapSort(arr):
-    n = len(arr)
-    swaps = []
-    for i in range(n//2 - 1, -1, -1):
-        heapify(arr, n, i, swaps)
-    for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  
-        swaps.append((0, i))
-        heapify(arr, i, 0, swaps)
-    return swaps
+class MinHeap:
+    def __init__(self, arr):
+        self.arr = arr
+        self.swaps = []
+        self.build_heap()
 
-try:
-    text = input().strip()
-    if "I" in text:
+    def build_heap(self):
+        n = len(self.arr)
+        for i in range(n // 2 - 1, -1, -1):
+            self.heapify(i)
+
+    def heapify(self, i):
+        smallest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < len(self.arr) and self.arr[left] < self.arr[smallest]:
+            smallest = left
+
+        if right < len(self.arr) and self.arr[right] < self.arr[smallest]:
+            smallest = right
+
+        if smallest != i:
+            self.swaps.append((i, smallest))
+            self.arr[i], self.arr[smallest] = self.arr[smallest], self.arr[i]
+            self.heapify(smallest)
+
+    def print_swaps(self):
+        print(len(self.swaps))
+        for x in self.swaps:
+            print(*x)
+
+if __name__ == "__main__":
+    source = input("source: ").strip().upper()
+
+    if source == "I":
         n = int(input())
         arr = list(map(int, input().split()))
-    elif "F" in text:
-        filename = input()
-        with open("tests/" + filename, 'r') as f:
-            n = int(f.readline())
-            arr = list(map(int, f.readline().split()))
-    else:
-        raise ValueError("Invalid input, please input F or I!")
-    if n != len(arr):
-        raise ValueError("Invalid input, length of data does not match!")
-    swaps = heapSort(arr)
-    print(len(swaps))
-    for s in swaps:
-        print(s[0], s[1])
+    elif source == "F":
+        filename = input("file name: ").strip()
 
-except ValueError:
-    print("Error")
+        assert filename.endswith(".a")
+
+        with open("tests/" + filename, "r") as file:
+            n = int(file.readline())
+            arr = list(map(int, file.readline().split()))
+    else:
+        raise ValueError("Invalid source")
+
+
+    assert len(arr) == n
+
+    min_heap = MinHeap(arr)
+    min_heap.print_swaps()
